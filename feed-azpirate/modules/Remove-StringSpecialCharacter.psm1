0508a5f2338c5d4cca52a540ef6394bdfcb2a5b9
Daniel McLoughlin <daniel_mcloughlin@outlook.com>
2023-04-25 12:22:26 +0100

    fix for á char in DB

--- feed-azpirate/modules/Remove-StringSpecialCharacter.psm1
+++ feed-azpirate/modules/Remove-StringSpecialCharacter.psm1
@@ -68,7 +68,7 @@ function Remove-StringSpecialCharacter {
 
             FOREACH ($Str in $string) {
                 Write-Verbose -Message "Original String: $Str"
-                $Str -replace $regex, "" -replace "å","a"
+                $Str -replace $regex, "" -replace "å","a" -replace "á","a"
             }
         }
         catch {
