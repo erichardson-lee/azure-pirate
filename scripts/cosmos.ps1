9794315bcc0fef0545e923ebe5c60880e9938a55
Daniel McLoughlin <daniel_mcloughlin@outlook.com>
2023-04-27 12:07:27 +0100

    testing files

--- scripts/cosmos.ps1
+++ scripts/cosmos.ps1
@@ -1,37 +1,73 @@
-# . env.ps1
+﻿# . env.ps1
 
 . "$PSScriptRoot\env.ps1"
 
 $SecureKey = ConvertTo-SecureString $env:NewCosmosAccountKey -AsPlainText -Force
 $cosmosDbContext = New-CosmosDbContext -Account $env:NewCosmosAccountName -Database $env:NewCosmosDBName -Key $SecureKey
-$query = "SELECT * FROM logs c"
+$query = "SELECT * FROM logs c WHERE c.handle = `"@kaylumah`""
 $record = Get-CosmosDbDocument -Context $cosmosDbContext -CollectionId $env:NewCosmosCollectionName -Query $query -QueryEnableCrossPartition $True # -MaxItemCount 5 # -ReturnJson
 
+# ForEach ($r in $record) {
+#     $r.author
+# }
 
-if ($record)
-{
-    $filtered = $record | Sort-Object date -Descending | select -first 10 | select handle, date, url, greeting #| ConvertTo-Json
-    ForEach ($r in $filtered)
-    {
-        if (!$r.title)
-        {
-            Write-Warning "This record has no title. Pulling it from greeting."
-            if ($r.greeting -and $r.greeting -match "called:")
-            {
-                $newTitle = $r.greeting.Split("called")[1].Split("Check it out it here")[0].Replace(": ","").Replace("`n","")
-                $r | Add-Member -Membertype NoteProperty -Name title -value $newTitle -Force
-            } else {
-                Write-Warning "This record has no title or usable greeting. ID: $($r.id). Skipping,"
-                continue
-            }
-        }
-    }
-} else {
-    Write-Error "No records returned. Stopping."
-    exit
+# $t = $record[-1].author
+
+$name = "Max Hamulyák" #-replace "å","a" -replace "á","a"
+
+# $enc = [System.Text.Encoding]::UTF8.GetBytes("á")
+
+# # $test = [System.Text.Encoding]::UTF8.GetString($enc)
+
+# # $test = [System.Text.Encoding]::ASCII.GetString($enc)
+
+# $test = [System.Text.Encoding]::Default.GetString($enc)
+
+$postDateFmt = Get-Date -Format "yyyy-MM-dd"
+
+$hash = [ordered]@{
+    id = "$([Guid]::NewGuid().ToString())";
+    date = $postDateFmt;
+    author = $name;
+    handle = "@kaylumah";
 }
 
-$filtered | select handle, date, url, title | ConvertTo-Json
+$message = $hash | ConvertTo-Json -Depth 4 | out-file "test.json" -encoding ascii
+
+$message = Get-Content "test.json" | ConvertTo-Json -encoding ascii
+
+New-CosmosDbDocument -Context $cosmosDbContext -CollectionId $env:NewCosmosCollectionName -DocumentBody $message -PartitionKey $postDateFmt
+
+
+
+
+# encode $name to utf8
+# $utf8 = [System.Text.Encoding]::UTF8.GetBytes($name)
+
+# if ($record)
+# {
+#     $filtered = $record | Sort-Object date -Descending | select -first 10 | select handle, date, url, greeting #| ConvertTo-Json
+#     ForEach ($r in $filtered)
+#     {
+#         if (!$r.title)
+#         {
+#             Write-Warning "This record has no title. Pulling it from greeting."
+#             if ($r.greeting -and $r.greeting -match "called:")
+#             {
+#                 $newTitle = $r.greeting.Split("called")[1].Split("Check it out it here")[0].Replace(": ","").Replace("`n","")
+#                 $r | Add-Member -Membertype NoteProperty -Name title -value $newTitle -Force
+#             } else {
+#                 Write-Warning "This record has no title or usable greeting. ID: $($r.id). Skipping,"
+#                 continue
+#             }
+#         }
+#     }
+# } else {
+#     Write-Error "No records returned. Stopping."
+#     exit
+# }
+
+# $filtered | select handle, date, url, title | ConvertTo-Json
 
 
 # $filtered = $record | Sort-Object date -Descending | select -first 10 | select handle, date, url, greeting | ConvertTo-Json
@@ -178,72 +214,72 @@ $filtered | select handle, date, url, title | ConvertTo-Json
 
 
 
-# https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/manage-with-powershell
+# # https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/manage-with-powershell
 
-$subscriptionName = "Visual Studio Enterprise Subscription - MCT"
+# $subscriptionName = "Visual Studio Enterprise Subscription - MCT"
 
-Connect-AzAccount -Subscription $subscriptionName
+# Connect-AzAccount -Subscription $subscriptionName
 
-$resourceGroupName = "myResourceGroup"
-$location = "UK South"
-$accountName = "mycosmosaccount"
-$apiKind = "Sql"
-$databaseName = "mydatabase"
-$containerName = "mycontainer"
-$partitionKeyPath = "/id"
-$throughput = 400 #minimum = 400
+# $resourceGroupName = "myResourceGroup"
+# $location = "UK South"
+# $accountName = "mycosmosaccount"
+# $apiKind = "Sql"
+# $databaseName = "mydatabase"
+# $containerName = "mycontainer"
+# $partitionKeyPath = "/id"
+# $throughput = 400 #minimum = 400
 
-New-AzResourceGroup -Name $resourceGroupName -Location $location
+# New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-New-AzCosmosDBAccount `
-    -Name $accountName `
-    -ResourceGroupName $resourceGroupName `
-    -Location $location `
-    -ApiKind $apiKind
+# New-AzCosmosDBAccount `
+#     -Name $accountName `
+#     -ResourceGroupName $resourceGroupName `
+#     -Location $location `
+#     -ApiKind $apiKind
 
-New-AzCosmosDBSqlDatabase `
-    -ResourceGroupName $resourceGroupName `
-    -AccountName $accountName `
-    -Name $databaseName    
+# New-AzCosmosDBSqlDatabase `
+#     -ResourceGroupName $resourceGroupName `
+#     -AccountName $accountName `
+#     -Name $databaseName    
     
-New-AzCosmosDBSqlContainer `
-    -ResourceGroupName $resourceGroupName `
-    -AccountName $accountName `
-    -DatabaseName $databaseName `
-    -Name $containerName `
-    -PartitionKeyKind Hash `
-    -PartitionKeyPath $partitionKeyPath `
-    -Throughput $throughput
+# New-AzCosmosDBSqlContainer `
+#     -ResourceGroupName $resourceGroupName `
+#     -AccountName $accountName `
+#     -DatabaseName $databaseName `
+#     -Name $containerName `
+#     -PartitionKeyKind Hash `
+#     -PartitionKeyPath $partitionKeyPath `
+#     -Throughput $throughput
 
-$key = (Get-AzCosmosDBAccountKey `
-    -ResourceGroupName $resourceGroupName `
-    -Name $accountName `
-    -Type "Keys").PrimaryMasterKey
+# $key = (Get-AzCosmosDBAccountKey `
+#     -ResourceGroupName $resourceGroupName `
+#     -Name $accountName `
+#     -Type "Keys").PrimaryMasterKey
 
-$requiredPSModules = "Az", "CosmosDB"
+# $requiredPSModules = "Az", "CosmosDB"
 
-ForEach ($module in $requiredPSModules)
-{
-    if (-not(Get-Module -ListAvailable -Name $module)) {
-        Install-Module $module
-        Import-Module $module # Might not be needed
-    }
-}
+# ForEach ($module in $requiredPSModules)
+# {
+#     if (-not(Get-Module -ListAvailable -Name $module)) {
+#         Install-Module $module
+#         Import-Module $module # Might not be needed
+#     }
+# }
 
-$document = [ordered]@{
-    id           = "$([Guid]::NewGuid().ToString())";
-    name         = "Dan";
-    location     = "Ripon";
-}
+# $document = [ordered]@{
+#     id           = "$([Guid]::NewGuid().ToString())";
+#     name         = "Dan";
+#     location     = "Ripon";
+# }
 
-0..9 | Foreach-Object {
-    $id = $([Guid]::NewGuid().ToString())
-    $document = @"
-{
-    `"id`": `"$id`",
-    `"content`": `"Some string`",
-    `"more`": `"Some other string`"
-}
-"@
-    New-CosmosDbDocument -Context $cosmosDbContext -CollectionId $containerName -DocumentBody $document -PartitionKey $id
-}
+# 0..9 | Foreach-Object {
+#     $id = $([Guid]::NewGuid().ToString())
+#     $document = @"
+# {
+#     `"id`": `"$id`",
+#     `"content`": `"Some string`",
+#     `"more`": `"Some other string`"
+# }
+# "@
+#     New-CosmosDbDocument -Context $cosmosDbContext -CollectionId $containerName -DocumentBody $document -PartitionKey $id
+# }
--- scripts/send-tweet.ps1
+++ scripts/send-tweet.ps1
@@ -13,31 +13,31 @@ Set-TwitterAuthentication `
     -AccessToken $AccessToken `
     -AccessTokenSecret $AccessTokenSecret
 
-# $PathToImage = "C:\Users\danm\Downloads\pirate.jpg"
-# $MediaId = (Send-TwitterMedia -Path $PathToImage -Category TweetImage -AltImageText 'A Pirate').media_id
+$PathToImage = "C:\Users\danm\Downloads\pirate.jpg"
+$MediaId = (Send-TwitterMedia -Path $PathToImage -Category TweetImage -AltImageText 'A Pirate').media_id
 
-# Publish-Tweet `
-#  -TweetText "Yarr! I be messing about with the Twitter API again!" `
-#  -MediaId $MediaId
+Publish-Tweet `
+ -TweetText "Just making sure I can post images with the v2 Twitter API" `
+ -MediaId $MediaId
 
 
 #  Publish-Tweet -TweetText "A new release of #BluebirdPS will soon be released. BluebirdPS is #PowerShell 7 Twitter automation client. Check it out! https://bit.ly/BluebirdPS"
 
-$msg = "Avast Ye!
+# $msg = "Avast Ye!
 
-New blog post from @jackwesleyroper called: OpenShift vs. Kubernetes: What is the Difference?
+# New blog post from @jackwesleyroper called: OpenShift vs. Kubernetes: What is the Difference?
 
-Check it out it here: https://url.azurepirate.com/sqr
+# Check it out it here: https://url.azurepirate.com/sqr
 
-#Azure #AzureFamily #CloudFamily #AzurePirate"
+# #Azure #AzureFamily #CloudFamily #AzurePirate"
 
-$msg = "Hit The Deck!
+# $msg = "Hit The Deck!
 
-New YouTube post from @NTFAQGuy called: Getting Started Creating Content
+# New YouTube post from @NTFAQGuy called: Getting Started Creating Content
 
-Check it out it here: https://youtube.com/watch?v=_TGq7Q8QMYk
+# Check it out it here: https://youtube.com/watch?v=_TGq7Q8QMYk
 
-#Azure #AzureFamily #CloudFamily #AzurePirate"
+# #Azure #AzureFamily #CloudFamily #AzurePirate"
 
 # $msgSplit = $msg.split('`n')
 # ForEach ($line in $msgSplit) { 
@@ -48,15 +48,15 @@ Check it out it here: https://youtube.com/watch?v=_TGq7Q8QMYk
 # }
 
 
-if ($msg -match 'youtube.com')
-{
-    write "video post"
-    $url = $msg | select-string -AllMatches '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)' | %{ $_.Matches.value } 
-    $urlId = $url.Split('v=')[-1]
-    $imgUrl = "https://img.youtube.com/vi/$($urlId)/sddefault.jpg"
-    # D:\home\
-    Invoke-WebRequest $imgUrl -OutFile "C:\Users\danm\Downloads\sddefault.jpg"
+# if ($msg -match 'youtube.com')
+# {
+#     write "video post"
+#     $url = $msg | select-string -AllMatches '(http[s]?|[s]?ftp[s]?)(:\/\/)([^\s,]+)' | %{ $_.Matches.value } 
+#     $urlId = $url.Split('v=')[-1]
+#     $imgUrl = "https://img.youtube.com/vi/$($urlId)/sddefault.jpg"
+#     # D:\home\
+#     Invoke-WebRequest $imgUrl -OutFile "C:\Users\danm\Downloads\sddefault.jpg"
     
-} else {
-    write "blog post"
-}
+# } else {
+#     write "blog post"
+# }
